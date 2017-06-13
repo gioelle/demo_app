710e6300e20376cd16bc7b79f9656d3c4f580cdb
Joelle.Fronzaglio <Joelle.Fronzaglio@VUHL-2YQBSF2>
2017-06-13 13:14:22 -0500

    Add project files.

---
+++ demo_app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            // git status
+            // git add .
+            // git commit -m "message"
+            // git push -u origin 
+
+            #endregion
+
+
+        }
+    }
+}
